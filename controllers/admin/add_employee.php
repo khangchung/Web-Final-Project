477ee5127035ee2749ac09ad77f33f562db2719b
unknown <glennchung2000@gmail.com>
2021-12-20 15:25:55 +0700

    Change type of position and gender variable in add_employee of admin controller, add 1 line into gitignore

--- controllers/admin/add_employee.php
+++ controllers/admin/add_employee.php
@@ -7,10 +7,10 @@
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
     $fullname = isset($_POST["fullname"]) ? $_POST["fullname"] : "";
     $gender = isset($_POST["gender"]) ? $_POST["gender"] : "";
-    $position = isset($_POST["position"]) ? $_POST["position"] : "";
+    $position = isset($_POST["position"]) ? intval($_POST["position"]) : "";
     $department = isset($_POST["department"]) ? $_POST["department"] : "";
     $avatar = isset($_POST["avatar"]) ? $_POST["avatar"] : "";
-    $day_off = isset($_POST["day_off"]) ? $_POST["day_off"] : "";
+    $day_off = isset($_POST["day_off"]) ? intval($_POST["day_off"]) : "";
 
     if (!empty($id) && !empty($username) && !empty($fullname) && !empty($gender) && !empty($position) 
     && !empty($department) && !empty($avatar) && !empty($day_off)) {
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("models/setup.php");
-    $datetime1 = '2017-05-1';
-    $datetime2 = '2017-05-20';
-    echo getDateDistance($datetime1, $datetime2);
-    echo password_hash(123456, PASSWORD_DEFAULT);
-?>
