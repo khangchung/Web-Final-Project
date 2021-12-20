4df4a360c367598fcf7708e9befbd83d1364d2f7
unknown <glennchung2000@gmail.com>
2021-12-20 15:33:11 +0700

    Add create account function into create employee function

--- controllers/admin/add_employee.php
+++ controllers/admin/add_employee.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     require_once("../../models/employee_operations.php");
+    require_once("../../models/account_operations.php");
     require_once("../../models/employee.php");
+    require_once("../../models/account.php");
 
     $id = isset($_POST["id"]) ? $_POST["id"] : "";
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
@@ -14,11 +16,18 @@
 
     if (!empty($id) && !empty($username) && !empty($fullname) && !empty($gender) && !empty($position) 
     && !empty($department) && !empty($avatar) && !empty($day_off)) {
-        $employee = new Employee($id, $username, $fullname, $gender, $position, $department, $avatar, 
-        $day_off);
-        $employeeOperations = new EmployeeOperations;
-        $result = $employeeOperations->create($employee);
-        $_SESSION["flag"] = $result;
+        $password = password_hash($username, PASSWORD_DEFAULT);
+        $AO = new AccountOperations;
+        $account_result = $AO->create(new Account($username, $password, $position));
+        if ($account_result) {
+            $employee = new Employee($id, $username, $fullname, $gender, $position, $department, $avatar, 
+            $day_off);
+            $employeeOperations = new EmployeeOperations;
+            $result = $employeeOperations->create($employee);
+            $_SESSION["flag"] = $result;    
+        } else {
+            $_SESSION["flag"] = $account_result;
+        }
     } else {
         $_SESSION["flag"] = false;
     }
