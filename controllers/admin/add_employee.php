060f05c7c3a297008fb340b9f54b54dd6b2a40a7
unknown <glennchung2000@gmail.com>
2021-12-20 16:41:51 +0700

    Drop gender attribute of employee in add and edit employee

--- controllers/admin/add_employee.php
+++ controllers/admin/add_employee.php
@@ -8,19 +8,18 @@
     $id = isset($_POST["id"]) ? $_POST["id"] : "";
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
     $fullname = isset($_POST["fullname"]) ? $_POST["fullname"] : "";
-    $gender = isset($_POST["gender"]) ? intval($_POST["gender"]) : "";
     $position = isset($_POST["position"]) ? intval($_POST["position"]) : "";
     $department = isset($_POST["department"]) ? $_POST["department"] : "";
     $avatar = isset($_POST["avatar"]) ? $_POST["avatar"] : "";
     $day_off = isset($_POST["day_off"]) ? intval($_POST["day_off"]) : "";
 
-    if (!empty($id) && !empty($username) && !empty($fullname) && !empty($gender) && !empty($position) 
+    if (!empty($id) && !empty($username) && !empty($fullname) && !empty($position) 
     && !empty($department) && !empty($avatar) && !empty($day_off)) {
         $password = password_hash($username, PASSWORD_DEFAULT);
         $accountOperations = new AccountOperations;
         $account_result = $accountOperations->create(new Account($username, $password, $position));
         if ($account_result) {
-            $employee = new Employee($id, $username, $fullname, $gender, $position, $department, $avatar, 
+            $employee = new Employee($id, $username, $fullname, $position, $department, $avatar, 
             $day_off);
             $employeeOperations = new EmployeeOperations;
             $result = $employeeOperations->create($employee);
--- controllers/admin/edit_employee.php
+++ controllers/admin/edit_employee.php
@@ -6,13 +6,12 @@
     $id = isset($_POST["id"]) ? $_POST["id"] : "";
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
     $fullname = isset($_POST["fullname"]) ? $_POST["fullname"] : "";
-    $gender = isset($_POST["gender"]) ? $_POST["gender"] : "";
     $position = isset($_POST["position"]) ? $_POST["position"] : "";
     $department = isset($_POST["department"]) ? $_POST["department"] : "";
     $avatar = isset($_POST["avatar"]) ? $_POST["avatar"] : "";
     $day_off = isset($_POST["day_off"]) ? $_POST["day_off"] : "";
 
-    if (!empty($id) && !empty($username) && !empty($fullname) && !empty($gender) && !empty($position) 
+    if (!empty($id) && !empty($username) && !empty($fullname) && !empty($position) 
     && !empty($department) && !empty($avatar) && !empty($day_off)) {
         $employee = new Employee($id, $username, $fullname, $gender, $position, $department, $avatar, 
         $day_off);
