fd6c83b10b821d11dd5b2e65b588f76e356b6c59
unknown <glennchung2000@gmail.com>
2021-12-20 16:29:54 +0700

    Change type of room variable in add department

--- controllers/admin/add_department.php
+++ controllers/admin/add_department.php
@@ -5,7 +5,7 @@
 
     $name = isset($_POST["name"]) ? $_POST["name"] : "";
     $desc = isset($_POST["desc"]) ? $_POST["desc"] : "";
-    $room = isset($_POST["room"]) ? $_POST["room"] : "";
+    $room = isset($_POST["room"]) ? intval($_POST["room"]) : "";
 
     if (!empty($name) && !empty($desc) && !empty($room)) {
         $department = new Department($name, $desc, $room);
