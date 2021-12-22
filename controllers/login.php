0657199c71ce507c9151601111ad3ecd31bca942
unknown <glennchung2000@gmail.com>
2021-12-22 08:26:14 +0700

    modify function

--- controllers/login.php
+++ controllers/login.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    $_SESSION["flag"] = false;
     require_once("../models/account_operations.php");
 
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
@@ -15,23 +17,29 @@
         if ($account->getUsername() == $username && $account->getPassword() == $password) {
         // if ($account->getUsername() == $username && password_verify($password, $account->getPassword())) {
             if ($account->getPriority() == 0) {
-                session_start();
                 $_SESSION["username"] = $username;
                 $_SESSION["priority"] = $priority;
+                $_SESSION["flag"] = "admin";
                 header("location: admin/index.php");
             } else
             if ($account->getPriority() == 1) {
-                session_start();
                 $_SESSION["username"] = $username;
                 $_SESSION["priority"] = $priority;
+                $_SESSION["flag"] = "monitor";
                 header("location: monitor/index.php");
             } else
             if ($account->getPriority() == 2) {
-                session_start();
                 $_SESSION["username"] = $username;
                 $_SESSION["priority"] = $priority;
+                $_SESSION["flag"] = "employee";
                 header("location: employee/index.php");
             }
         }
+    }
+
+    if (isset($_SESSION["flag"])) {
+        header("location: employee/" . $_SESSION["flag"] . ".php");
+    } else {
+        header("location: ../../views/login.php");
     }
 ?>
