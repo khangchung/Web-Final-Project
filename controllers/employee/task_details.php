4c28a19cdabe8212ba7a383d01bce2865de852ef
unknown <glennchung2000@gmail.com>
2021-12-24 18:00:11 +0700

    modify way of getting task details

--- controllers/employee/task_details.php
+++ controllers/employee/task_details.php
@@ -3,9 +3,12 @@
     require_once("../../models/task_operations.php");
     $id = isset($_GET["id"]) ? $_GET["id"] : "";
     if (!empty($id)) {
-        $taskOperations = new TaskOperations;
-        $taskManager = $taskOperations->read_one($id);
-        $_SESSION["task"] = $taskManager->getList()[0];
+        foreach ($_SESSION["task"] as $task) {
+            if ($task->getId() == $id) {
+                $_SESSION["task_details"] = $task;
+                break;
+            }
+        }
         header("location: ../../views/employee/details_task.php");    
     } else {
         $_SESSION["flag"] = false;
