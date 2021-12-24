552d6fbfeca4bc6b0daeeec1def1be11580ca41f
unknown <glennchung2000@gmail.com>
2021-12-24 14:47:34 +0700

    Add offday.php

---
+++ controllers/employee/offday.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    require_once("../../models/absence_operations.php");
+    $absenceOperations = new AbsenceOperations;
+    $absenceManager = $absenceOperations->read();
+    $absenceList = $absenceManager->getList();
+    $data = array();
+    foreach ($absenceList as $absence) {
+        if ($absence->getEmployeeId() == $_SESSION["information"]->getId()) {
+            array_push($data, $absence);
+        }
+    }
+    $_SESSION["absence"] = $data;
+    header("location: ../../views/employee/offday_manager.php");
+?>
