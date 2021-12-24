5ae3476ac697474d1203ef7133353eaf10fb4b64
unknown <glennchung2000@gmail.com>
2021-12-24 15:13:19 +0700

    modify code

--- models/upload.php
+++ models/upload.php
@@ -2,6 +2,9 @@
     // options may be id task or absence
     function upload($tmp_path, $department_name, $employee_name, $option) {
         $path = "../../documents/" . $department_name . "/" . $employee_name . "/" . $option;
+        if (!file_exists($path)) {
+            mkdir($path);
+        }
         move_uploaded_file($tmp_path, $path);
         return $path;
     }
