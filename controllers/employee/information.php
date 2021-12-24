7ff4b3a8ee015be930036f8f56e73e71c49117e1
unknown <glennchung2000@gmail.com>
2021-12-24 14:38:41 +0700

    delete a few code

--- controllers/employee/information.php
+++ controllers/employee/information.php
@@ -1,14 +1,4 @@
 <?php
     session_start();
-    require_once("../../models/employee_operations.php");
-    $employeeOperations = new EmployeeOperations;
-    $employeeManager = $employeeManager->read();
-    $employeeList = $employeeManager->getList();
-    foreach ($employeeList as $employee) {
-        if ($employee->getUsername() == $_SESSION["username"]) {
-            $_SESSION["information"] = $employee;
-            break;
-        }
-    }
     header("location: ../../views/employee/information.php");
 ?>
