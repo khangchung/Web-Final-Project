459aa199d2ce7383a64c9930c667edcbe8c1c250
unknown <glennchung2000@gmail.com>
2021-12-24 14:04:03 +0700

    append code

--- controllers/employee/index.php
+++ controllers/employee/index.php
@@ -1,4 +1,8 @@
 <?php
     session_start();
+    require_once("../../models/task_operations.php");
+    $taskOperations = new TaskOperations;
+    $taskManager =  $taskOperations->read();
+    $_SESSION["task"] = $taskManager->getList();
     header("location: ../../views/employee/index.php");
 ?>
