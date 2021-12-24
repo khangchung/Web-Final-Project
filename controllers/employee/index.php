9b08dca666d7082bc59e80a6ee966cf3e3479fe9
unknown <glennchung2000@gmail.com>
2021-12-24 14:37:59 +0700

    append getting user infor function

--- controllers/employee/index.php
+++ controllers/employee/index.php
@@ -1,6 +1,17 @@
 <?php
     session_start();
+    require_once("../../models/employee_operations.php");
     require_once("../../models/task_operations.php");
+    
+    $employeeOperations = new EmployeeOperations;
+    $employeeManager = $employeeManager->read();
+    $employeeList = $employeeManager->getList();
+    foreach ($employeeList as $employee) {
+        if ($employee->getUsername() == $_SESSION["username"]) {
+            $_SESSION["information"] = $employee;
+            break;
+        }
+    }
     $taskOperations = new TaskOperations;
     $taskManager =  $taskOperations->read();
     $_SESSION["task"] = $taskManager->getList();
