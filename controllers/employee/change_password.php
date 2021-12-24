75f268dd3bddf2258d0742191706e8adc43356e1
unknown <glennchung2000@gmail.com>
2021-12-24 18:36:24 +0700

    modify code

--- controllers/employee/change_password.php
+++ controllers/employee/change_password.php
@@ -2,14 +2,20 @@
     session_start();
     require_once("../../models/account_operations.php");
     require_once("../../models/account.php");
-    $username = isset($_POST["username"]) ? $_POST["username"] : "";
-    $password = isset($_POST["password"]) ? $_POST["password"] : "";
-    if (!empty($username) && !empty($password)) {
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $account = new Account($username, $password, 2);
+    $old_password = isset($_POST["old_password"]) ? $_POST["old_password"] : "";
+    $new_password = isset($_POST["new_password"]) ? $_POST["new_password"] : "";
+    if (!empty($old_password) && !empty($new_password)) {
         $accountOperations = new AccountOperations;
-        $result = $accountOperations->update($account);
-        $_SESSION["flag"] = $result;
+        $accountManager = $accountOperations->read_one($_SESSION["username"]);
+        $account = $accountManager->getList()[0];
+        if ($account->getPassword() == $old_password) {
+            $password = password_hash($new_password, PASSWORD_DEFAULT);
+            $account->setPassword($password);
+            $result = $accountOperations->update($account);
+            $_SESSION["flag"] = $result;    
+        } else {
+            $_SESSION["flag"] = false;
+        }
         header("location: ../../views/employee/change_password.php");
     } else {
         $_SESSION["flag"] = false;
