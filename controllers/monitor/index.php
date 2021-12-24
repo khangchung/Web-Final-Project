d82df1eb64b4b912b4365068e7f6650ea0a114d5
unknown <glennchung2000@gmail.com>
2021-12-24 18:02:17 +0700

    Append code

--- controllers/monitor/index.php
+++ controllers/monitor/index.php
@@ -1,4 +1,26 @@
 <?php
     session_start();
+    require_once("../../models/employee_operations.php");
+    require_once("../../models/task_operations.php");
+    
+    $employeeOperations = new EmployeeOperations;
+    $employeeManager = $employeeOperations->read();
+    $employeeList = $employeeManager->getList();
+    foreach ($employeeList as $employee) {
+        if ($employee->getUsername() == $_SESSION["username"]) {
+            $_SESSION["information"] = $employee;
+            break;
+        }
+    }
+    $taskOperations = new TaskOperations;
+    $taskManager =  $taskOperations->read();
+    $taskList = $taskManager->getList();
+    $data = array();
+    foreach ($taskList as $task) {
+        if ($task->getCreator() == $_SESSION["information"]->getId()) {
+            array_push($data, $task);
+        }
+    }
+    $_SESSION["task"] = $data;
     header("location: ../../views/monitor/index.php");
 ?>
