b94283f64a0812898df6db2f7c4cab2926553e5e
unknown <glennchung2000@gmail.com>
2021-12-26 21:35:36 +0700

    Change censored function

--- controllers/admin/censored.php
+++ controllers/admin/censored.php
@@ -2,18 +2,11 @@
     session_start();
     require_once("../../models/absence_operations.php");
 
-    $id = isset($_POST["id"]) ? intval($_POST["id"]) : "";
-    $employee_id = isset($_POST["employee_id"]) ? $_POST["employee_id"] : "";
-    $created_date = isset($_POST["created_date"]) ? $_POST["created_date"] : "";
-    $start_date = isset($_POST["start_date"]) ? $_POST["start_date"] : "";
-    $end_date = isset($_POST["end_date"]) ? $_POST["end_date"] : "";
-    $reason = isset($_POST["reason"]) ? $_POST["reason"] : "";
-    $attachment = isset($_POST["attachment"]) ? $_POST["attachment"] : "";
+    $id = isset($_GET["id"]) ? intval($_GET["id"]) : "";
     
-    if (!empty($employee_id)) {
-        $absence = new Absence($id, $employee_id, $created_date, $start_date, $end_date, $reason, 1, $attachment);
+    if (!empty($id)) {
         $absenceOperations = new AbsenceOperations;
-        $result = $absenceOperations->update($absence);
+        $result = $absenceOperations->update($id);
         $_SESSION["flag"] = $result;
         header("location: ../../views/admin/offday_manager.php");
     } else {
