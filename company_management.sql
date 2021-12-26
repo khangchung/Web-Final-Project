d05d1edb46d48c30aef0b6d99c60046ea5c8bce9
unknown <glennchung2000@gmail.com>
2021-12-26 22:26:29 +0700

    new query for creating database

--- company_management.sql
+++ company_management.sql
@@ -1,234 +1,76 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 26, 2021 at 12:25 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.1.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `company_management`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `absence`
---
-
-CREATE TABLE `absence` (
-  `id` int(5) NOT NULL,
-  `employee_id` varchar(4) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_date` date DEFAULT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  `num_of_day` int(2) NOT NULL,
-  `reason` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` int(1) DEFAULT NULL,
-  `attachment` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `account`
---
-
 CREATE TABLE `account` (
-  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `priority` int(1) DEFAULT 2
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `account`
---
-
-INSERT INTO `account` (`username`, `password`, `priority`) VALUES
-('admin', '$2y$10$UKNuwtMlJFaCMwOSAu07buy/ZHfHzN.eBxi5PpYjtP3N81eqbDjLe', 0),
-('employee', '$2y$10$UsLAZ0tULAWrI/6cYiGUB.GjIIV.ppS3JsOrLz2xlvKiR38McsiYq', 2),
-('monitor', '$2y$10$AOt3x4UOaclo.fE3FJ5o8uvsB2Q0Az19jbtsDxvyqkQ0Cq6FRcLYa', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `department`
---
-
-CREATE TABLE `department` (
-  `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `room` int(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `department`
---
-
-INSERT INTO `department` (`name`, `description`, `room`) VALUES
-('Administration', 'Phụ trách hành chính', 5),
-('Analysis', 'Phụ trách phân tích', 2),
-('Business', 'Phụ trách kinh doanh', 1),
-('Design', 'Phụ trách thiết kế', 3),
-('IT', 'Phụ trách lập trình', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employee`
---
+	`username` varchar(50) NOT NULL,
+	`password` varchar(255),
+	`priority` int(1) DEFAULT '2',
+	PRIMARY KEY (`username`)
+);
 
 CREATE TABLE `employee` (
-  `id` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `fullname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `position` int(1) DEFAULT NULL,
-  `department` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `avatar` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `day_off` int(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+	`id` char(5) NOT NULL,
+	`username` varchar(50),
+	`fullname` varchar(50),
+	`gender` int,
+	`position` varchar(20),
+	`department` varchar(20),
+	`avatar` varchar(100),
+	`day_off` int(2),
+	PRIMARY KEY (`id`)
+);
 
---
--- Dumping data for table `employee`
---
-
-INSERT INTO `employee` (`id`, `username`, `fullname`, `position`, `department`, `avatar`, `day_off`) VALUES
-('A002', 'employee', 'employee', 2, 'Analysis', '', 12),
-('D003', 'monitor', 'monitor', 1, 'Design', '', 15);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `task`
---
+CREATE TABLE `department` (
+	`name` varchar(20) NOT NULL,
+	`description` TEXT,
+	`room` int(2),
+	PRIMARY KEY (`name`)
+);
 
 CREATE TABLE `task` (
-  `id` int(5) NOT NULL,
-  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `description` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` int(1) DEFAULT NULL,
-  `rate` int(1) DEFAULT NULL,
-  `creator` varchar(4) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `receiver` varchar(4) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_date` date DEFAULT NULL,
-  `deadline` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+	`id` int(5) NOT NULL AUTO_INCREMENT,
+	`title` varchar(100),
+	`description` TEXT,
+	`status` int(1),
+	`rate` int(1),
+	`creator` char(5),
+	`receiver` char(5),
+	`created_date` DATE,
+	`deadline` DATE,
+	PRIMARY KEY (`id`)
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `task_log`
---
+CREATE TABLE `absence` (
+	`id` int(5) NOT NULL AUTO_INCREMENT,
+	`employee` char(5),
+	`created_date` DATE,
+	`start_date` DATE,
+	`end_date` DATE,
+	`reason` TEXT,
+	`status` int(1),
+	`attachment` varchar(100),
+	PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `task_log` (
-  `task_id` int(5) NOT NULL,
-  `comment` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `attachment` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+	`task_id` int(5) NOT NULL,
+	`comment` varchar(100) NOT NULL,
+	`attachment` varchar(100) NOT NULL,
+	PRIMARY KEY (`task_id`,`comment`,`attachment`)
+);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE `employee` ADD CONSTRAINT `employee_fk0` FOREIGN KEY (`username`) REFERENCES `account`(`username`);
 
---
--- Indexes for table `absence`
---
-ALTER TABLE `absence`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `absence_fk0` (`employee_id`);
+ALTER TABLE `employee` ADD CONSTRAINT `employee_fk1` FOREIGN KEY (`department`) REFERENCES `department`(`name`);
 
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`username`);
+ALTER TABLE `task` ADD CONSTRAINT `task_fk0` FOREIGN KEY (`creator`) REFERENCES `employee`(`id`);
 
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`name`);
+ALTER TABLE `task` ADD CONSTRAINT `task_fk1` FOREIGN KEY (`receiver`) REFERENCES `employee`(`id`);
 
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `employee_fk0` (`username`),
-  ADD KEY `employee_fk1` (`department`);
+ALTER TABLE `absence` ADD CONSTRAINT `absence_fk0` FOREIGN KEY (`employee`) REFERENCES `employee`(`id`);
 
---
--- Indexes for table `task`
---
-ALTER TABLE `task`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `task_fk0` (`creator`),
-  ADD KEY `task_fk1` (`receiver`);
+ALTER TABLE `task_log` ADD CONSTRAINT `task_log_fk0` FOREIGN KEY (`task_id`) REFERENCES `task`(`id`);
 
---
--- Indexes for table `task_log`
---
-ALTER TABLE `task_log`
-  ADD PRIMARY KEY (`task_id`,`comment`,`attachment`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `absence`
---
-ALTER TABLE `absence`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `task`
---
-ALTER TABLE `task`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `absence`
---
-ALTER TABLE `absence`
-  ADD CONSTRAINT `absence_fk0` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
 
---
--- Constraints for table `employee`
---
-ALTER TABLE `employee`
-  ADD CONSTRAINT `employee_fk0` FOREIGN KEY (`username`) REFERENCES `account` (`username`),
-  ADD CONSTRAINT `employee_fk1` FOREIGN KEY (`department`) REFERENCES `department` (`name`);
-
---
--- Constraints for table `task`
---
-ALTER TABLE `task`
-  ADD CONSTRAINT `task_fk0` FOREIGN KEY (`creator`) REFERENCES `employee` (`id`),
-  ADD CONSTRAINT `task_fk1` FOREIGN KEY (`receiver`) REFERENCES `employee` (`id`);
-
---
--- Constraints for table `task_log`
---
-ALTER TABLE `task_log`
-  ADD CONSTRAINT `task_log_fk0` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
