c078e5c61f0097a5e954783b276e8fc1025507cd
unknown <glennchung2000@gmail.com>
2021-12-28 21:20:17 +0700

    Add upload avatar function

--- models/upload.php
+++ models/upload.php
@@ -8,4 +8,13 @@
         move_uploaded_file($tmp_path, $path);
         return $path;
     }
+
+    function uploadAvatar($tmp_path, $department_name, $employee_name) {
+        $path = "../../documents/" . $department_name . "/" . $employee_name;
+        if (!file_exists($path)) {
+            mkdir($path);
+        }
+        move_uploaded_file($tmp_path, $path);
+        return $path;
+    }
 ?>
