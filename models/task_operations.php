d60a3d8c48401de1e792fd40a125cfd8ba039123
unknown <glennchung2000@gmail.com>
2021-12-29 16:13:26 +0700

    Fix bug

--- models/task_operations.php
+++ models/task_operations.php
@@ -56,6 +56,7 @@
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     $task = new Task(
+                        $row["id"],
                         $row["title"],
                         $row["description"],
                         $row["status"],
