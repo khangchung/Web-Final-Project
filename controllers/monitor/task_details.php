012b85fb13fa50bf7740bb30abbab534f5ffd1ff
unknown <glennchung2000@gmail.com>
2021-12-29 20:55:23 +0700

    Add new file

---
+++ controllers/monitor/task_details.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require_once("../../models/task_operations.php");
+    
+    $id = isset($_GET["id"]) ? $_GET["id"] : "";
+    if (!empty($id)) {
+        foreach ($_SESSION["tasks"] as $task) {
+            $task = unserialize($task);
+            if ($task->getId() == $id) {
+                $_SESSION["task"] = serialize($task);
+                break;
+            }
+        }
+        header("location: ../../views/monitor/details_task.php");    
+    } else {
+        $_SESSION["flag"] = false;
+        header("location: ../../views/monitor/index.php");
+    }
+?>
