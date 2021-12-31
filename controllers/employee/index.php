596ea582e63ec998e4d979fe0e4132b28579b009
unknown <glennchung2000@gmail.com>
2021-12-31 19:38:32 +0700

    Fix bug

--- controllers/employee/index.php
+++ controllers/employee/index.php
@@ -8,13 +8,11 @@
     $employeeList = $employeeManager->getList();
 
     $information = isset($_SESSION["information"]) ? $_SESSION["information"] : "";
-    if (!empty($information)) {
-        foreach ($employeeList as $employee) {
-            $employee = unserialize($employee);
-            if ($employee->getUsername() == $_SESSION["username"]) {
-                $_SESSION["information"] = serialize($employee);
-                break;
-            }
+    foreach ($employeeList as $employee) {
+        $employee = unserialize($employee);
+        if ($employee->getUsername() == $_SESSION["username"]) {
+            $_SESSION["information"] = serialize($employee);
+            break;
         }
     }
     $taskOperations = new TaskOperations;
