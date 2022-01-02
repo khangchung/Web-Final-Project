271169da3623adfa9da6cef6a39043865d4142e5
unknown <glennchung2000@gmail.com>
2022-01-02 22:50:31 +0700

    Fix bugs

--- controllers/employee/index.php
+++ controllers/employee/index.php
@@ -8,36 +8,39 @@
     $employeeManager = $employeeOperations->read();
     $employeeList = $employeeManager->getList();
 
+    foreach ($employeeList as $employee) {
+        $employee = unserialize($employee);
+        if ($employee->getUsername() == $_SESSION["username"]) {
+            $_SESSION["information"] = serialize($employee);
+            break;
+        }
+    }
+
     if (isset($_SESSION["information"])) {
-        foreach ($employeeList as $employee) {
-            $employee = unserialize($employee);
-            if ($employee->getUsername() == $_SESSION["username"]) {
-                $_SESSION["information"] = serialize($employee);
-                break;
+        $taskOperations = new TaskOperations;
+        $taskManager =  $taskOperations->read();
+        $taskList = $taskManager->getList();
+        $data = array();
+        foreach ($taskList as $task) {
+            $task = unserialize($task);
+            if ($task->getReceiver() == unserialize($_SESSION["information"])->getId()) {
+                array_push($data, serialize($task));
             }
-        }   
-    }
-    $taskOperations = new TaskOperations;
-    $taskManager =  $taskOperations->read();
-    $taskList = $taskManager->getList();
-    $data = array();
-    foreach ($taskList as $task) {
-        $task = unserialize($task);
-        if ($task->getReceiver() == unserialize($_SESSION["information"])->getId()) {
-            array_push($data, serialize($task));
         }
-    }
-    $_SESSION["tasks"] = $data;
-    $tasklogOperations = new TaskLogOperations;
-    $tasklogManager =  $tasklogOperations->read();
-    $tasklogList = $tasklogManager->getList();
-    $data2 = array();
-    foreach ($tasklogList as $tasklog) {
-        $tasklog = unserialize($tasklog);
-        if ($task->getReceiver() == unserialize($_SESSION["information"])->getId()) {
-            array_push($data2, serialize($tasklog));
+        $_SESSION["tasks"] = $data;
+        $tasklogOperations = new TaskLogOperations;
+        $tasklogManager =  $tasklogOperations->read();
+        $tasklogList = $tasklogManager->getList();
+        $data2 = array();
+        foreach ($tasklogList as $tasklog) {
+            $tasklog = unserialize($tasklog);
+            if ($task->getReceiver() == unserialize($_SESSION["information"])->getId()) {
+                array_push($data2, serialize($tasklog));
+            }
         }
+        $_SESSION["task_logs"] = $data2;
+        header("location: ../../views/employee/index.php");
+    } else {
+        header("location: ../../views/login.php");
     }
-    $_SESSION["task_logs"] = $data2;
-    header("location: ../../views/employee/index.php");
 ?>
--- controllers/monitor/index.php
+++ controllers/monitor/index.php
@@ -7,37 +7,39 @@
     $employeeManager = $employeeOperations->read();
     $employeeList = $employeeManager->getList();
 
-    if (isset($_SESSION["information"])) {
-        foreach ($employeeList as $employee) {
-            $employee = unserialize($employee);
-            if ($employee->getUsername() == $_SESSION["username"]) {
-                $_SESSION["information"] = serialize($employee);
-                break;
-            }
-        }
-    }
-    $employee_data = array();
     foreach ($employeeList as $employee) {
         $employee = unserialize($employee);
-        if ($employee->getDepartment() == unserialize($_SESSION["information"])->getDepartment()
-            && $employee->getId() != unserialize($_SESSION["information"])->getId()) {
-            array_push($employee_data, serialize($employee));
+        if ($employee->getUsername() == $_SESSION["username"]) {
+            $_SESSION["information"] = serialize($employee);
+            break;
         }
     }
-    $_SESSION["employees"] = $employee_data;
-    $taskOperations = new TaskOperations;
-    $taskManager =  $taskOperations->read();
-    $taskList = $taskManager->getList();
-    $task_data = array();
-    foreach ($taskList as $task) {
-        $task = unserialize($task);
-        if ($task->getCreator() == unserialize($_SESSION["information"])->getId()) {
-            array_push($task_data, serialize($task));
+
+    if (isset($_SESSION["information"])) {
+        $employee_data = array();
+        foreach ($employeeList as $employee) {
+            $employee = unserialize($employee);
+            if ($employee->getDepartment() == unserialize($_SESSION["information"])->getDepartment()
+                && $employee->getId() != unserialize($_SESSION["information"])->getId()) {
+                array_push($employee_data, serialize($employee));
+            }
         }
+        $_SESSION["employees"] = $employee_data;
+        $taskOperations = new TaskOperations;
+        $taskManager =  $taskOperations->read();
+        $taskList = $taskManager->getList();
+        $task_data = array();
+        foreach ($taskList as $task) {
+            $task = unserialize($task);
+            if ($task->getCreator() == unserialize($_SESSION["information"])->getId()) {
+                array_push($task_data, serialize($task));
+            }
+        }
+        $_SESSION["tasks"] = $task_data;
+        if (isset($_SESSION["employees"]) && isset($_SESSION["tasks"])) {
+            header("location: ../../views/monitor/index.php");
+        }
+    } else {
+        header("location: ../../views/login.php");
     }
-    $_SESSION["tasks"] = $task_data;
-    if (isset($_SESSION["employees"]) && isset($_SESSION["tasks"])) {
-        header("location: ../../views/monitor/index.php");
-    }
-    
 ?>
