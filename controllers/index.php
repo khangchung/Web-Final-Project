d7f25f750237e726c3befd971d2bbf2092f3f9ce
unknown <glennchung2000@gmail.com>
2022-01-12 18:40:58 +0700

    Add new feature

--- controllers/index.php
+++ controllers/index.php
@@ -1,3 +1,16 @@
 <?php
-    header("location: ../views/login.php");
+    session_start();
+    if (isset($_SESSION["username"]) && isset($_SESSION["priority"])) {
+        if ($_SESSION["priority"] == 0) {
+            header("location: admin/index.php");
+        } else
+        if ($_SESSION["priority"] == 1) {
+            header("location: monitor/index.php");
+        } else
+        if ($_SESSION["priority"] == 2) {
+            header("location: employee/index.php");
+        }
+    } else {
+        header("location: ../views/login.php");
+    }
 ?>
