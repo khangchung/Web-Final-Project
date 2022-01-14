57e129108cc7c5eef0d6cd305463ba4e670f17c8
unknown <glennchung2000@gmail.com>
2022-01-14 23:21:31 +0700

    Add sql file for new database

--- company_management.sql
+++ company_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 13, 2022 at 11:13 AM
+-- Generation Time: Jan 14, 2022 at 05:19 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.1.1
 
@@ -38,15 +38,6 @@ CREATE TABLE `absence` (
   `attachment` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `absence`
---
-
-INSERT INTO `absence` (`id`, `employee`, `created_date`, `start_date`, `end_date`, `reason`, `status`, `attachment`) VALUES
-(3, 'A004', '2022-01-06', '2022-01-07', '2022-01-10', 'abc', -1, '../../documents/A/A004/absense/2022-01-06.docx'),
-(4, 'A003', '2022-01-06', '2022-01-08', '2022-01-12', 'xyz', -1, '../../documents/A/A003/absense/2022-01-06.pdf'),
-(5, 'A003', '2022-01-13', '2022-01-14', '2022-01-16', 'Bận công việc gia đình', 0, '');
-
 -- --------------------------------------------------------
 
 --
@@ -64,11 +55,32 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`username`, `password`, `priority`) VALUES
-('admin', '$2y$10$1zhwkV0aXcQK1vUa8cWAX.0CjryPY.jT3chm/Z1VNl9Cj60PgMGZG', 0),
-('nguyenthia', '$2y$10$jRyBSJMa/vDoGe7AbyfRu..OxJFjxNDf6PtZoi6wasG53qOIBAHQW', 1),
-('nguyenthib', '$2y$10$AqrrI1EJ7PNa9WJIcXwFNuJD89MtRvyoLuMIYG5n2/f9NjXcf7B7u', 2),
-('tranvana', '$2y$10$iv/fIJbZtM9grAlpiI/E/eEoLKHEZ0QYk4Rw4l2Y.K24SXJBJ84jO', 1),
-('tranvanb', '$2y$10$p2Ia.FnbDLkfxShRKCqsveebhQ2khNrlUu9PzopimNXxf8qRi5GTm', 2);
+('admin', '$2y$10$wPPydT7wuMsSvAVQunRb3.i2B4hdEQuQSNmJvPKXzfjgMFdiW/gRG', 0),
+('buitrucphuong', '$2y$10$kRqOX2/YlObRdZuhSq.qNOEJZgh.3o9GqMB/NCzF0CzWf1A4mozQy', 2),
+('caothitrucquynh', '$2y$10$LRjztqAyWYmXDp8H/tTWpOPurRG4vkPjhy4XTT8mBlswshfsze4vy', 2),
+('chauthanhtoan', '$2y$10$keauji6Zm.Uo7vDV/W7MeegGBZeCtDZPgrkxvrV52rMtRI3WVa8RO', 2),
+('duongthithuyvy', '$2y$10$QNBWKVbPFFMMFyL8YcwkNOSjHLw2ByAkZIxdGqXjAib7kdhGA2406', 2),
+('hongoctuyet', '$2y$10$VBj3vPV87d.V.WFvS/o8M.46KRwW4Pojx8dQa3IvOdIwTpfhVppyK', 2),
+('huynhlong', '$2y$10$5RYWzmllS/m8iP905Bu5aeuoGBKwMyG.tfr2Y2mDnv0pfmLRGtw7K', 2),
+('huynhthanhtrieu', '$2y$10$q1C3UhtlRlU0Y49RPgKrGuVLjY/zQBUKMBnviFFAs.IJ/mTK81d72', 2),
+('huynhvankiet', '$2y$10$4..qZII6EViGZbRxquOu.Oz8iHW.dP0iJiopIvs.2zaCWoMddNSLy', 2),
+('lamvotrongnhan', '$2y$10$IDpdiaSm7bsbjRveiWIIc.gxYjdBG0eaj3JLl7RZQD2n1YJvARUAG', 2),
+('letruchan', '$2y$10$6AErDExjSQvpOTHgu4ikAuQ/Vzs0Y.QM33t.KgWHNXzneFh3MHiq2', 2),
+('levohoaibao', '$2y$10$fEENOBqyTGoYNqZu8.YHUOJKpr37jCmAkNp33B1.2wMsUC1pAzsZC', 2),
+('lythithuhuong', '$2y$10$f7jL0SYpeTffOjtSN731meYvG1Oc5/sxBwHkzItz99QR7MwmzsHga', 2),
+('nguyenhoaingoc', '$2y$10$tYSgNgI6V7ds6MImEnt1i..uBB1axKkrv02v.8Oa15dKcBtXdQg4K', 2),
+('nguyenphamkhang', '$2y$10$6zr8j8ree.9aQBBw554FaeIwXmS2t/ps1Q7viLMPdKWLwGyXETPgu', 2),
+('nguyenvanthai', '$2y$10$K8pgMCifaeawUADIEgcUDOcJul7KUJ/F8meU2wzACgh4d2MKQX/zy', 2),
+('phamtam', '$2y$10$MmUC5S0ueQN8pKB9FvILK.aXn4xP3C3IBnf5bNtSLh8bf1sveHi4u', 2),
+('trannguyenphuongthi', '$2y$10$3aDX7219edW0TKj9w/9QZekefhi.DPrZCkdd61FqdHRzdEsGrzmXy', 2),
+('tranthithaoanh', '$2y$10$IMeoRFf6pg9LOQB/JSCgg.tPanK4VhYfSqhfMw/iw9OmIyWu9LHta', 1),
+('tranvantuan', '$2y$10$bj8Axxz6XfZ8gVUn1cbAwOR77dpjUg3LPsdN2RWnEsBEc96UH1KRK', 2),
+('trinhtuyetvan', '$2y$10$IyWZcgpb5.LpVO2MKWFrIe6l2pbwHi7X45ngXZfC1b9Z/o2zUDYwa', 2),
+('vocatphuong', '$2y$10$xL.2fGHxqaLSXcG4yGxft.qBztesvRS7luNlcLxBGtvT6/6dxhQnW', 2),
+('vothanhlong', '$2y$10$fv3.eclmgZ1o9dYnIeKFXezCTDppCaxDZ6gSw6XVLTah7G/gsZGNG', 2),
+('vuducmanh', '$2y$10$xEzvNrevp8Ffx3GE.2X/MuktJe.JfgLbIrbZzC3cG1GwIG2.DbQ7C', 2),
+('vulanminhtri', '$2y$10$sW2bjnLkwQkkT31Zr6nuV.mXtvD29JIdKAkPLFtb0Li2UBAfSg2Fy', 2),
+('vungocha', '$2y$10$Q9ess2y7Qr4OfpMzwfgoT.DFNwHlQGwcgtAng8Hn3xzZ6IQj.Sjmq', 2);
 
 -- --------------------------------------------------------
 
@@ -88,8 +100,11 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`id`, `name`, `description`, `room`) VALUES
-('A', 'Nhân sự', 'Phụ trách nhân sự công ty', 1),
-('B', 'Thiết kế', 'Phụ trách thiết kế sản phẩm', 2);
+('A', 'Nhân sự', 'Phụ trách các công việc liên quan đến tổ chức, sắp xếp, điều động nhân sự công ty', 1),
+('B', 'Thiết kế', 'Phụ trách các công việc liên quan đến thiết kế trong công ty', 2),
+('C', 'Truyền thông', 'Phụ trách truyền thông sản phẩm, dịch vụ của công ty trên các nền tảng internet', 3),
+('D', 'Kinh doanh', 'Phụ trách các công việc liên quan đến phân tích thị trường, chiến lược kinh doanh', 4),
+('E', 'Kỹ thuật', 'Phụ trách các công việc liên quan đến xây dựng, bảo trì, nghiên cứu sản phẩm', 5);
 
 -- --------------------------------------------------------
 
@@ -112,10 +127,31 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`id`, `username`, `fullname`, `position`, `department`, `avatar`, `day_off`) VALUES
-('A003', 'tranvana', 'Trần Văn A', '1', 'A', '../../documents/A/A003/avatar.jpg', 15),
-('A004', 'tranvanb', 'Trần Văn B', '2', 'A', '../../documents/A/A004/avatar.jpg', 12),
-('B001', 'nguyenthia', 'Nguyễn Thị A', '1', 'B', '../../documents/B/B001/avatar.jpg', 15),
-('B002', 'nguyenthib', 'Nguyễn Thị B', '2', 'B', '../../documents/B/B002/avatar.jpg', 12);
+('A001', 'tranthithaoanh', 'Trần Thị Thảo Anh', '1', 'A', '../../documents/A/A001/avatar.jpg', 15),
+('A002', 'tranvantuan', 'Trần Văn Tuấn', '2', 'A', '../../documents/A/A002/avatar.jpg', 12),
+('A003', 'vocatphuong', 'Võ Cát Phượng', '2', 'A', '../../documents/A/A003/avatar.jpg', 12),
+('A004', 'trannguyenphuongthi', 'Trần Nguyễn Phương Thi', '2', 'A', '../../documents/A/A004/avatar.jpg', 12),
+('A005', 'levohoaibao', 'Lê Võ Hoài Bảo', '2', 'A', '../../documents/A/A005/avatar.jpg', 12),
+('B006', 'duongthithuyvy', 'Dương Thị Thúy Vy', '1', 'B', '../../documents/B/B006/avatar.jpg', 15),
+('B007', 'caothitrucquynh', 'Cao Thị Trúc Quỳnh', '2', 'B', '../../documents/B/B007/avatar.jpg', 12),
+('B008', 'lamvotrongnhan', 'Lâm Võ Trọng Nhân', '2', 'B', '../../documents/B/B008/avatar.jpg', 12),
+('B009', 'nguyenhoaingoc', 'Nguyễn Hoài Ngọc', '2', 'B', '../../documents/B/B009/avatar.jpg', 12),
+('B010', 'chauthanhtoan', 'Châu Thanh Toàn', '2', 'B', '../../documents/B/B010/avatar.jpg', 12),
+('C011', 'letruchan', 'Lê Trúc Hân', '1', 'C', '../../documents/C/C011/avatar.jpg', 15),
+('C012', 'huynhvankiet', 'Huỳnh Văn Kiệt', '2', 'C', '../../documents/C/C012/avatar.jpg', 12),
+('C013', 'nguyenvanthai', 'Nguyễn Văn Thái', '2', 'C', '../../documents/C/C013/avatar.jpg', 12),
+('C014', 'trinhtuyetvan', 'Trịnh Tuyết Vân', '2', 'C', '../../documents/C/C014/avatar.jpg', 12),
+('C015', 'vothanhlong', 'Võ Thành Long', '2', 'C', '../../documents/C/C015/avatar.jpg', 12),
+('D016', 'phamtam', 'Phạm Tâm', '1', 'D', '../../documents/D/D016/avatar.jpg', 15),
+('D017', 'vungocha', 'Vũ Ngọc Hà', '2', 'D', '../../documents/D/D017/avatar.jpg', 12),
+('D018', 'hongoctuyet', 'Hồ Ngọc Tuyết', '2', 'D', '../../documents/D/D018/avatar.jpg', 12),
+('D019', 'vulanminhtri', 'Vũ Lân Minh Trí', '2', 'D', '../../documents/D/D019/avatar.jpg', 12),
+('D020', 'lythithuhuong', 'Lý Thị Thu Hương', '2', 'D', '../../documents/D/D020/avatar.jpg', 12),
+('E021', 'huynhthanhtrieu', 'Huỳnh Thanh Triều', '1', 'E', '../../documents/E/E021/avatar.jpg', 15),
+('E022', 'nguyenphamkhang', 'Nguyễn Phạm Khang', '2', 'E', '../../documents/E/E022/avatar.jpg', 12),
+('E023', 'buitrucphuong', 'Bùi Trúc Phương', '2', 'E', '../../documents/E/E023/avatar.jpg', 12),
+('E024', 'huynhlong', 'Huỳnh Long', '2', 'E', '../../documents/E/E024/avatar.jpg', 12),
+('E025', 'vuducmanh', 'Vũ Đức Mạnh', '2', 'E', '../../documents/E/E025/avatar.jpg', 12);
 
 -- --------------------------------------------------------
 
@@ -137,15 +173,6 @@ CREATE TABLE `task` (
   `attachment` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `task`
---
-
-INSERT INTO `task` (`id`, `title`, `description`, `status`, `rate`, `creator`, `receiver`, `created_date`, `deadline`, `last_modified`, `attachment`) VALUES
-(1, 'Tuyển dụng nhân sự', 'Tuyển dụng 5 nhân viên thiết kế', 3, 0, 'A003', 'A004', '2022-01-11', '2022-01-14', '2022-01-13', ''),
-(2, 'Sắp xếp nhân sự', 'Sắp xếp vị trí nhân được tuyển dụng vào phòng ban thiết kế ', 2, 0, 'A003', 'A004', '2022-01-12', '2022-01-14', '2022-01-13', ''),
-(3, 'Điều động nhân sự', 'Điều động nhân sự phòng thiết kế hỗ trợ phòng kỹ thuật', 0, 0, 'A003', 'A004', '2022-01-12', '2022-01-13', '2022-01-13', '');
-
 -- --------------------------------------------------------
 
 --
@@ -158,13 +185,6 @@ CREATE TABLE `task_log` (
   `attachment` varchar(100) NOT NULL,
   `owner` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `task_log`
---
-
-INSERT INTO `task_log` (`task_id`, `comment`, `attachment`, `owner`) VALUES
-(1, 'Đã hoàn thành', '../../documents/A/A004/task/1/2022-01-11-tasklog.docx', 0);
 
 --
 -- Indexes for dumped tables
