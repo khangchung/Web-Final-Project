c34466f30871ac2c72dc84ea3c7b529c6dbbce68
unknown <glennchung2000@gmail.com>
2022-01-14 19:57:09 +0700

    Modify code writeToErrorLog function

--- models/setup.php
+++ models/setup.php
@@ -183,7 +183,7 @@
     }
 
     function writeToErrorLog($log_message, $page_message) {
-        $_SESSION["page_message_error"] = $page_message;
+        $_SESSION["page_message_error"] = $log_message;
         error_log($log_message);
         header("location: ../../views/error_page.php");
     }
